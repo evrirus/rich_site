ce2866b2c364d87e80c661d86a045de821ddbe27
evrirus <97801370+evrirus@users.noreply.github.com>
2024-06-24 23:55:00 +0300

    Добавил модели но они не подключены

--- richMachine/users/forms.py
+++ richMachine/users/forms.py
@@ -3,18 +3,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import CustomUser  # Или использовать django.contrib.auth.models.User
-
-
-
-class SignUpForm(UserCreationForm):
-    # Добавьте здесь любые дополнительные поля формы, если нужно
-    # Например:
-    # email = forms.EmailField()
-
-    class Meta:
-        model = CustomUser  # Или django.contrib.auth.models.User
-        fields = ('username', 'password1', 'password2')  # Поля, которые нужно заполнить при регистрации
+# Или использовать django.contrib.auth.models.User
 
 
 # users/forms.py
