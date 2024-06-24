7db619fd6bce6523c9d220b90cafbeb21df62c90
evrirus <97801370+evrirus@users.noreply.github.com>
2024-06-24 23:33:17 +0300

    оно запоминает ник терь
    УРАААААААААААААААААААА

--- richMachine/richMachine/urls.py
+++ richMachine/richMachine/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', include('homePage.urls')),
+    path('', include('homePage.urls', namespace='homePage')),
     path('users/', include('users.urls', namespace='users')),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
