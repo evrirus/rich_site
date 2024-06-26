d9b9b30f6aea2618e9c1d332bce0c362e637cc3a
evrirus <97801370+evrirus@users.noreply.github.com>
2024-06-26 19:32:17 +0300

    СДЕЛАЛ ЛОГИН И ЛОГАУТ

--- richMachine/homePage/views.py
+++ richMachine/homePage/views.py
@@ -1,40 +1,14 @@
 from django.shortcuts import render
 from utils import coll, get_district_by_id, get_house_by_id
 from icecream import ic
+from django.core.handlers.wsgi import WSGIRequest
+
 
 # Create your views here.
-def index(request):
+def index(request: WSGIRequest):
+    
     data = {
         'title': "Rich Machine",
+        'username': request.user.username
     }
     return render(request, 'homePage/index.html', data)
-
-# def profile(request):
-#     ic(request.user.username)
-    # user = coll.find_one({'user_id': 899827113})
-    
-    # houses_id = user['house']['houses']
-    # houses = []
-    # for x in houses_id:
-    #     house_info = get_house_by_id(x['id'])
-    #     district_info = get_district_by_id(house_info['district_id'])
-    #     house_info['district_info'] = district_info
-    #     houses.append(house_info)
-    
-    # data = {
-    #     "nickname": user.get('nickname'),
-    #     "money": user.get('money'),
-    #     "donate_balance": user.get('donate_balance'),
-    #     "job": user.get('job', {}).get('title') if user.get('job', {}).get('title') else 'Безработный',
-    #     "car": user.get('car'),
-    #     "yacht": user.get('yacht'),
-    #     "houses": houses,
-    #     "couple": None,
-    #     "registration": user.get('registration'),
-    #     "language": user.get('language'),
-    #     "active": user.get('active'),
-    #     "username": user.get('username')
-    # }
-    # data = {'nickname': {'name': request.user.username}}
-    
-    # return render(request, 'homePage/profile.html', data)
--- richMachine/users/urls.py
+++ richMachine/users/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+
 from . import views
 
 app_name = 'users'
@@ -6,4 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/<str:username>/', views.profile, name='profile'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),
 ]
