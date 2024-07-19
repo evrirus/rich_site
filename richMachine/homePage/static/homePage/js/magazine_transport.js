8a597f2d885096874dfb7cbf2290848e8015a96e
evrirus <97801370+evrirus@users.noreply.github.com>
2024-07-20 00:15:06 +0300

    сделал загрузку

--- richMachine/homePage/static/homePage/js/magazine_transport.js
+++ richMachine/homePage/static/homePage/js/magazine_transport.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const transportItemsContainer = document.querySelector('.property'); 
     const modal = document.querySelector('#transportModal');
     const acceptBuyTransportButton = document.getElementById('accept_buy_transport');
+    const infoCar = modal.querySelector('.info__car');
 
     // Создаем объект кеша
     const cache = {};
 
     let currentTransportType = null;
     let currentTransportId = null;
-    
+
     if (transportItemsContainer && modal) {
         transportItemsContainer.addEventListener('click', (event) => {
             const transportItem = event.target.closest('.transport_item');
@@ -18,6 +19,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         function openModal(event, item) {
+            const loadingIndicator = modal.querySelector('.loading');
+            const infoCar = modal.querySelector('.info__car'); // New container for the actual content
+            
+            loadingIndicator.style.visibility = 'visible';
+            infoCar.style.visibility = 'hidden'; // Hide the content initially
+            modal.style.visibility = 'visible';
+
             const id = item.getAttribute('data-modal-id');
             const type = item.getAttribute('data-type');
             currentTransportType = type;
@@ -44,20 +52,26 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
             
-            modal.style.visibility = 'visible';
             modal.style.left = `${event.layerX}px`; 
             modal.style.top = `${event.layerY}px`; 
         }
 
         function updateModalContent(data) {
+            const loadingIndicator = modal.querySelector('.loading');
+
             document.querySelector('.transport-name').textContent = `${data.name}`;
             document.querySelector('.transport-price').textContent = `${data.price} ₽`;
             document.querySelector('.transport-produced').textContent = `Произведено: ${data.produced}`;
             document.querySelector('.transport-quantity').textContent = `В наличии: ${data.quantity}`;
+            
+            loadingIndicator.style.visibility = 'hidden';
+            infoCar.style.visibility = 'visible'; // Show the content once loaded
+            modal.style.visibility = 'visible';
         }
 
         function closeModal() {
             modal.style.visibility = 'hidden';
+            infoCar.style.visibility = 'hidden';
         }
 
         window.addEventListener('click', (event) => {
@@ -69,8 +83,6 @@ document.addEventListener('DOMContentLoaded', () => {
         // Добавляем обработчик для кнопки покупки
         if (acceptBuyTransportButton) {
             acceptBuyTransportButton.addEventListener('click', function() {
-                const loadingIndicator = modal.querySelector('.loading');
-                loadingIndicator.style.display = 'block'; // Показываем индикатор загрузки
 
                 const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
@@ -86,18 +98,29 @@ document.addEventListener('DOMContentLoaded', () => {
                     })
                 }).then(response => response.json())
                   .then(data => {
-                      loadingIndicator.style.display = 'none'; // Скрываем индикатор загрузки
-
-                      if (data.success) {
-                          alert('Покупка прошла успешно!');
-                          closeModal();
-                      } else {
-                          alert('Произошла ошибка при покупке: ' + data.message);
-                      }
-                  }).catch(error => {
-                      loadingIndicator.style.display = 'none'; // Скрываем индикатор загрузки
-                      alert('Произошла ошибка: ' + error.message);
-                  });
+                        if (data.success) {
+                            alert('Покупка прошла успешно!');
+                            closeModal();
+                        } else {
+                            alert('Произошла ошибка при покупке: ' + data.message);
+                            // Запрашиваем разрешение на уведомления
+                            Notification.requestPermission().then(permission => {
+                                if (permission === "granted") {
+                                    console.log('Пытаюсь создать уведомление');
+                                    const notification = new Notification('Rich Site', {
+                                        body: data.message,
+                                        // icon: '/static/homePage/img/logo.png'
+                                    });
+                                } else {
+                                    console.log('Разрешение на уведомления не получено');
+                                }
+                            }).catch(err => {
+                                console.error("Ошибка при получении разрешения на уведомления:", err);
+                            });
+                        }
+                }).catch(error => {
+                    alert('Произошла ошибка: ' + error.message);
+                });
             });
         } else {
             console.error('Кнопка покупки не найдена');
