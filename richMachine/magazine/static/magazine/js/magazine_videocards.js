fe1b3b6d29a127c8d4c334b7e79e0f044f6fc635
evrirus <97801370+evrirus@users.noreply.github.com>
2024-08-08 00:34:55 +0300

    сделал отображение ячеек в инвентаре

--- richMachine/magazine/static/magazine/js/magazine_videocards.js
+++ richMachine/magazine/static/magazine/js/magazine_videocards.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const transportItemsContainer = document.querySelector('.property'); 
-    const modal = document.querySelector('#transportModal');
-    const acceptBuyTransportButton = document.getElementById('accept_buy_transport');
-    const infoCar = modal.querySelector('.info__car');
-
+    const videocardItemsContainer = document.querySelector('.property'); 
+    const modal = document.querySelector('#videocardModal');
+    const acceptBuyVideocardButton = document.getElementById('accept_buy_videocard');
+    const infoCard = modal.querySelector('.info__videocard');
     const loadingIndicator = modal.querySelector('.loader');
-
     // Создаем объект кеша
     const cache = {};
-
-    let currentTransportType = null;
-    let currentTransportId = null;
 
     function handleMessages(messages) {
         const container = $('#messages-container');
@@ -37,25 +32,27 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    if (transportItemsContainer && modal) {
-        transportItemsContainer.addEventListener('click', (event) => {
-            const transportItem = event.target.closest('.transport_item');
-            if (transportItem) {
-                openModal(event, transportItem);
+    let currentVideocardId = null;
+    
+    if (videocardItemsContainer && modal) {
+        videocardItemsContainer.addEventListener('click', (event) => {
+            const videocardItem = event.target.closest('.videocard_item');
+            if (videocardItem) {
+                openModal(event, videocardItem);
             }
         });
 
         function openModal(event, item) {
-            // const infoCar = modal.querySelector('.info__car'); // New container for the actual content
             
+
             loadingIndicator.style.visibility = 'visible';
-            infoCar.style.visibility = 'hidden'; // Hide the content initially
+            infoCard.style.visibility = 'hidden'; 
             modal.style.visibility = 'visible';
 
             const id = item.getAttribute('data-modal-id');
             const type = item.getAttribute('data-type');
-            currentTransportType = type;
-            currentTransportId = id;
+
+            currentVideocardId = id;
             const cacheKey = `${type}-${id}`;  // Ключ для кеша
 
             // Проверяем кеш
@@ -65,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Подгрузка данных из БД с использованием AJAX
                 $.ajax({
                     type: 'GET',
-                    url: `get_transport_info/${type}/${id}/`,  // URL вашего API эндпоинта
+                    url: `get_videocard_info/${id}/`,  // URL вашего API эндпоинта
                     success: function(response) {
                         // Сохраняем данные в кеш
                         cache[cacheKey] = response;
@@ -74,68 +71,72 @@ document.addEventListener('DOMContentLoaded', () => {
                     },
                     error: function(xhr, status, error) {
                         console.error('Ошибка при загрузке данных: ', error);
-                        
                     }
-                    
                 });
             }
-            
             modal.style.left = `${event.layerX}px`; 
             modal.style.top = `${event.layerY}px`; 
+            
         }
 
         function updateModalContent(data) {
+            console.log(data);
+            document.querySelector('.info-videocard-name').textContent = `${data.name}`;
+            document.querySelector('.info-videocard-price').textContent = `${data.price} $`;
+            document.querySelector('.info-videocard-performance').textContent = `Доход ${data.performance}$  / день`;
 
-            document.querySelector('.transport-name').textContent = `${data.name}`;
-            document.querySelector('.transport-price').textContent = `${data.price} ₽`;
-            document.querySelector('.transport-produced').textContent = `Произведено: ${data.produced}`;
-            document.querySelector('.transport-quantity').textContent = `В наличии: ${data.quantity}`;
-            
+             
             loadingIndicator.style.visibility = 'hidden';
-            infoCar.style.visibility = 'visible'; // Show the content once loaded
+            infoCard.style.visibility = 'visible';
             modal.style.visibility = 'visible';
         }
 
         function closeModal() {
             modal.style.visibility = 'hidden';
-            infoCar.style.visibility = 'hidden';
+            infoCard.style.visibility = 'hidden';
             loadingIndicator.style.visibility = 'hidden';
         }
 
         window.addEventListener('click', (event) => {
-            if (!modal.contains(event.target) && !event.target.closest('.transport_item')) {
+            if (!modal.contains(event.target) && !event.target.closest('.videocard_item')) {
                 closeModal();
             }
         });
 
         // Добавляем обработчик для кнопки покупки
-        if (acceptBuyTransportButton) {
-            acceptBuyTransportButton.addEventListener('click', function() {
+        if (acceptBuyVideocardButton) {
+            acceptBuyVideocardButton.addEventListener('click', function() {
+
+                loadingIndicator.style.visibility = 'visible'; // Показываем индикатор загрузки
 
                 const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
-                fetch(`/magazine/buy_transport/${currentTransportType}/${currentTransportId}/`, {
+                fetch(`./buy_videocard/${currentVideocardId}/`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                         'X-CSRFToken': csrfToken
                     },
                     body: JSON.stringify({
-                        type: currentTransportType,
-                        id: currentTransportId
+                        id: currentVideocardId
                     })
                 }).then(response => response.json())
                   .then(data => {
-                        closeModal();
-                        handleMessages(data.messages)
-                }).catch(error => {
-                    alert('Произошла ошибка: ' + error.message);
-                });
+                      loadingIndicator.style.visibility = 'hidden'; // Скрываем индикатор загрузки
+
+                      if (data.success) {
+                          closeModal();
+                      } 
+                      handleMessages(data.messages)
+                  }).catch(error => {
+                      loadingIndicator.style.visibility = 'hidden'; // Скрываем индикатор загрузки
+                      
+                  });
             });
         } else {
             console.error('Кнопка покупки не найдена');
         }
     } else {
-        console.error('Один из элементов не найден: transportItemsContainer или modal');
+        console.error('Один из элементов не найден: videocardItemsContainer или modal');
     }
 });
